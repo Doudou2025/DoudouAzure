1bf563fac8a42a2dccf46eeb271d3396f4d01e34
doudou <mamadousocio93@gmail.com>
2026-09-20 06:43:27 +0200

    Added unit tests

---
+++ src/test/java/HelloDoudou.java
@@ -0,0 +1,12 @@
+import junit.framework.TestCase;
+
+public class HelloDoudou extends TestCase {
+
+    public void testOne() {
+        assertTrue(true);
+    }
+
+    public void testTwo() {
+        assertTrue(true);
+    }
+}
